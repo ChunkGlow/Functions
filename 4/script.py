0126be399e3b3746919489b35efedadbe3711beb
ChunkGlow <memoguy34@gmail.com>
2025-04-03 16:07:05 +0800

    GH

---
+++ 4/script.py
@@ -0,0 +1,11 @@
+a = input("Enter a word: ")
+
+for i in a:
+    if (i == 'h'):
+        print("H is found")  
+
+        break 
+
+
+    else:
+        print ("H not found")
---
+++ 4/script3.py
@@ -0,0 +1,10 @@
+var = 100
+
+while var > 0:
+  var = var - 1
+  if var == 5:
+      break
+
+print ('\nCurrent variable value :', var)
+print ("\nGood bye!")
+
